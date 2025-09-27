2bed329827abf42ce4fb833fb60f8026d55b2f5d
rafzzzhosting-source <rafzzzhosting@gmail.com>
2025-09-27 11:51:28 +0700

    Update send.js

--- api/send.js
+++ api/send.js
@@ -1,1 +1,23 @@
+export default async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).json({error:'Method not allowed'});
+  }
 
+  const token = '8384066749:AAHXenoUSt9EprnzHP1RGv9XSDRhPxthGvM'; // ganti
+  const chat_id = '8331405438'; // ganti
+
+  const { text } = req.body;
+
+  try {
+    const r = await fetch(`https://api.telegram.org/bot${token}/sendMessage`, {
+      method:'POST',
+      headers:{'Content-Type':'application/json'},
+      body: JSON.stringify({chat_id,text})
+    });
+    const d = await r.json();
+    res.status(200).json(d);
+  } catch(e) {
+    console.error(e);
+    res.status(500).json({error:'Gagal kirim ke Telegram'});
+  }
+      }
